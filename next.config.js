5834c6f373ded97b6495f51f0de4200e8f51751c
tyoshida-maker <t.yoshida@classic.co.jp>
2026-04-28 10:19:09 +0900

    fix: rename pdf-generator.ts to tsx, fix JSX syntax error

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
-    return config;
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...(config.externals || []), 
+        '@react-pdf/renderer',
+        'canvas'
+      ]
+    }
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      canvas: false,
+    }
+    return config
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@react-pdf/renderer'],
   },
 }
 module.exports = nextConfig
